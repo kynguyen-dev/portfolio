86e58acc0e1abd5be63d5378aaf41bd233789dbf
Ky Nguyen <ky.nguyen@ptnglobalcorp.com>
2024-03-22 16:44:31 +0700

    feat: init meterial ui and font family

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,33 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Button, Link, Stack, Typography } from '@mui/material';
 
 function App() {
   const [count, setCount] = useState(0);
-  const myData = 'asds';
-  console.log(myData);
+
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
+      <Stack direction={'row'} display={'flex'} justifyContent={'center'}>
+        <Link href='https://vitejs.dev' target='_blank'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
+        </Link>
+        <Link href='https://react.dev' target='_blank'>
           <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
+        </Link>
+      </Stack>
+      <Typography variant='h2'>Vite + React</Typography>
+      <Stack padding={'2em'}>
+        <Button onClick={() => setCount(count => count + 1)}>
           count is {count}
-        </button>
-        <p>
+        </Button>
+        <Typography variant='body1'>
           Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
+        </Typography>
+      </Stack>
+      <Typography variant='body1' color={'#888'}>
         Click on the Vite and React logos to learn more
-      </p>
+      </Typography>
     </>
   );
 }
